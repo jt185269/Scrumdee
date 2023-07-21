81566698721c14751396fb34ed9e3eb763056841
James Taylor <jt185269@ncr.com>
2023-07-21 18:18:10 +0100

    soz

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = 5000
 const bodyParser = require('body-parser')
 const users = require('./users.json')
+const fs = require('fs')
 
 
 app.use(cors())
@@ -32,6 +33,35 @@ app.get('/users', (req, res) => {
     res.send(users)
 })
 
+app.get('/retro/categories', (req, res) => {
+    console.log("jncaca")
+    fs.readFile('categories.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            let obj = JSON.parse(data); //now it an object
+            console.log(obj)
+            res.send(obj)
+        }
+    });
+
+})
+
+app.post('/retro/categories', (req, res) => {
+    let newCat = req.body
+    console.log(newCat)
+    fs.readFile('categories.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            let obj = JSON.parse(data); //now it an object
+            obj.push(newCat); //add some data
+            let json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile('categories.json', json, 'utf8', () => res.sendStatus(200)); // write it back
+            res.send(obj)
+        }
+    });
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
