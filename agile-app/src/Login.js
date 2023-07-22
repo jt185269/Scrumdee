995499cfa1e485186bca30aa6a9d4920d55cc74f
Maddie Wilson <33693004+mb185390@users.noreply.github.com>
2023-07-22 14:44:42 +0100

    Updated login page... definitely not the right way to do it but it looks a little better

--- agile-app/src/Login.js
+++ agile-app/src/Login.js
@@ -1,13 +1,13 @@
 import './Login.css';
 import { useState } from "react"
-import  Form  from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Image from "react-bootstrap/Image";
 
 
-export const Login = ({onAuth}) => {
+export const Login = ({ onAuth }) => {
     const [username, setUsername] = useState("")
     const [pword, setPword] = useState("")
 
@@ -18,25 +18,30 @@ export const Login = ({onAuth}) => {
         })
     }
     return (
-        <Container className="login-Form">   
-        <Form>
-            <Row className="Row-Stuff">
-                <Form.Group>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control  onChange={(e) => setUsername(e.target.value)} value={username} type="text"/>
-                </Form.Group>
-            </Row>
-            
-            <Row className="Row-Stuff">
-                <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control onChange={(e) => setPword(e.target.value)} value={pword} type="password"/>
-                </Form.Group>
-            </Row>
-                <Row className="button">
-                <Button onClick={onSubmit} type='submit'>Log in!</Button>
+        <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Row><Image src="https://upload.wikimedia.org/wikipedia/commons/f/f0/NCR_logo_black.svg" ></Image></Row>
+            <Row className='logo'>Atleos Agililty</Row>
+            <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Form>
+                <Row className="Row-Stuff">
+                    <Form.Group>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control onChange={(e) => setUsername(e.target.value)} value={username} type="text" />
+                    </Form.Group>
+                </Row>
+
+                <Row className="Row-Stuff">
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control onChange={(e) => setPword(e.target.value)} value={pword} type="password" />
+                    </Form.Group>
+                </Row>
+
+                <Row className="button Row-Stuff" >
+                    <Button onClick={onSubmit} type='submit' style={{ justifyContent: 'center', alignItems: 'center'}}>Log in</Button>
                 </Row>
             </Form>
+            </Container>
 
         </Container>
     )
